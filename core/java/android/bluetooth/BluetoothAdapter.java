bec0395fcf3d00c393d2bf6a23a3c3b429bb8455
Steve Kondik <shade@chemlab.org>
2011-05-01 18:48:04 -0400

    wimax: Remove CM WiMAX support in favor of Google's
    Change-Id: I73ad5433b1357eb12300d1470fa22c432b468341

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -1000,13 +1000,4 @@ public final class BluetoothAdapter {
         }
         return true;
     }
-
-    /**
-     * For compatibility with proprietary code
-     *
-     * @hide
-     */
-    public boolean setAFH(boolean paramBoolean, byte paramByte1, byte paramByte2) {
-        return true;
-    }
 }
